53347fa381992086999550bd3caff2da9538bff0
Tariq Khokhar <tariq@khokhar.net>
2016-09-23 13:42:10 -0400

    ternary scale and SVG map

--- rmaps/maps.R
+++ rmaps/maps.R
@@ -1,72 +1,53 @@
-
-# use ggsave to output svg file. 
+# Make an untitled ternary SVG map for inclusion in print graphics
 
 library(maptools)
 library(WDI)
 library(rgdal)
+library(svglite)
+library(Cairo)
 
-#use basic world map from maptools
+#Use basic world map from maptools and project to winkel tripel
 
 data("wrld_simpl")
+proj4string(wrld_simpl) <- CRS("+proj=longlat")
+winkel <- "+proj=wintri"
+countries_winkel <- spTransform(wrld_simpl, CRS(winkel))
+par(mar=c(0,0,0,0))
 
 #get data on forest protected areas from WDI and join with map data on ISO code
 
 forestdata <- WDI(country = "all", indicator = "ER.LND.PTLD.ZS", start = 2014, end = 2014)
 forestdata.o <- forestdata[match(wrld_simpl$ISO2,forestdata$iso2c),]
 
-# create vector of map colors based on data
-mapCol <- c()
-for (i in 1:length(forestdata.o$ER.LND.PTLD.ZS)){
-  if (!is.na(forestdata.o$ER.LND.PTLD.ZS[i])){  
-    if(forestdata.o$ER.LND.PTLD.ZS[i] > 20){
-      mapCol <- c(mapCol,"#d95f0e")
-    } else if(forestdata.o$ER.LND.PTLD.ZS[i] > 5 & forestdata.o$ER.LND.PTLD.ZS[i] < 20){
-      mapCol <- c(mapCol,"#fec44f")
+
+
+#Colorbrewer Colors
+#ffeda0 <- light orange
+#feb24c <- medium orange
+#f03b20 <- dark orange
+
+findColor3 <- function(x) {
+ if (!is.na(x)){
+     if (x < 5) {
+      col <- "#ffeda0"
+    } else if (x > 5 & x < 20) {
+      col <- "#feb24c"
+    } else if (x > 20) {
+      col <- "#f03b20"
     } else {
-      mapCol <- c(mapCol,"#fff7bc")
+      col <- "black" #catch any screw ups
     }
-  } else{
-    mapCol <- c(mapCol,"#ccccc")
-  }
+    return(col)
+ }
+  col <-"white" #shade nulls
+  return(col)
 }
 
-mapCol2 <- c()
-for (i in 1:length(forestdata.o$ER.LND.PTLD.ZS)){
-  if (!is.na(forestdata.o$ER.LND.PTLD.ZS[i])){
-    mapCol2 <- c(mapCol2,"red")
-  } else {
-    mapCol2 <- c(mapCol2,"blue")
-  }
-}
-
-for (i in 1:length(forestdata.o$ER.LND.PTLD.ZS)){
-    if(forestdata.o$ER.LND.PTLD.ZS[i]>50){
-      mapCol <- c(mapCol,"#d95f0e")
-    } else {
-      mapCol <- c(mapCol,"#fff7bc")
-    }
-}
-
-if(forestdata.o$ER.LND.PTLD.ZS[2]>5){
-  mapCol <- c(mapCol,"#d95f0e")
-} else {
-  mapCol <- c(mapCol,"#fff7bc")
-}
-
-
-# World map
-proj4string(wrld_simpl) <- CRS("+proj=longlat")
-winkel <- "+proj=wintri"
-countries_winkel <- spTransform(wrld_simpl, CRS(winkel))
-par(mar=c(0,0,0,0))
-plot(countries_winkel, col=mapCol, lwd=0.3)
-
+ternaryColors <- sapply(forestdata.o$ER.LND.PTLD.ZS, findColor3)
 
 svg(filename="output.svg", 
     width=25, 
     height=20, 
     pointsize=12)
-plot(countries_winkel, col=mapCol, lwd=0.3)
+plot(countries_winkel, col=ternaryColors, lwd=0.3)
 dev.off()
-
-
